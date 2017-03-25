168e84ace82b5747566ee1a0808ed09ba1b0ab81
Tiago Amado Durante <tiagodurante@icloud.com>
2017-03-25 14:57:24 -0300

    * .gitignore: Primeiro commit
    * Main.cs:
    * Info.plist:
    * AppDelegate.cs:
    * MeuPrimeiroApp.sln:
    * packages.config:
    * MainActivity.cs:
    * packages.config:
    * Entitlements.plist:
    * App.xaml:
    * App.xaml.cs:
    * MyPage.xaml:
    * LaunchScreen.storyboard:
    * AboutAssets.txt:
    * MyPage.xaml.cs:
    * MeuPrimeiroApp.iOS.csproj:
    * packages.config:
    * MyPageModal.xaml:
    * AssemblyInfo.cs:
    * Contents.json:
    * MeuPrimeiroApp.Droid.csproj:
    * styles.xml:
    * icon.png:
    * AboutResources.txt:
    * MyPageModal.xaml.cs:
    * Tabbar.axml:
    * Toolbar.axml:
    * Resource.designer.cs:
    * MeuPrimeiroApp.csproj:
    * AndroidManifest.xml:
    * MeuPrimeiroAppPage.xaml:
    * icon.png:
    * icon.png:
    * icon.png:
    * MeuPrimeiroAppPage.xaml.cs:
    * AssemblyInfo.cs:
    * Contents.json:

---
+++ MeuPrimeiroApp/MeuPrimeiroAppPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Xamarin.Forms;
+
+namespace MeuPrimeiroApp
+{
+	public partial class MeuPrimeiroAppPage : ContentPage
+	{
+		public MeuPrimeiroAppPage()
+		{
+			//if (Device.OS == TargetPlatform.iOS)
+			//{
+			//	Padding = new Thickness(0, 20, 0, 0);
+			//}
+
+			Padding = Device.OnPlatform(new Thickness(20), new Thickness(0), new Thickness(0));
+
+			Title = "EURO TREINO";
+			InitializeComponent();
+		}
+
+		void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			//DisplayAlert("Resposta", "A lata", "Obrigado Nathan");
+			Navigation.PushAsync(new MyPage());
+		}
+
+		void Handle_Clicked_ARREGAO(object sender, System.EventArgs e)
+		{
+			//DisplayAlert("Resposta", "A lata", "Obrigado Nathan");
+			Navigation.PushModalAsync(new MyPageModal());
+		}
+	}
+}
---
+++ MeuPrimeiroApp/MyPageModal.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MeuPrimeiroApp
+{
+	public partial class MyPageModal : ContentPage
+	{
+		public MyPageModal()
+		{
+			InitializeComponent();
+		}
+
+		void Comeca_deNovo(object sender, System.EventArgs e)
+		{
+			//DisplayAlert("Resposta", "A lata", "Obrigado Nathan");
+			Navigation.PopModalAsync();
+		}
+	}
+}
